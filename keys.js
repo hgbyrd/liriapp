1ddba46f837bc95e309aacf967c0431b9bcd59b3
Garren Byrd <garrenbyrd@Garrens-MacBook-Pro.local>
2017-07-05 23:19:37 -0700

    init

---
+++ keys.js
@@ -0,0 +1,9 @@
+
+console.log('LIRI IS RUNNING');
+
+exports.twitterKeys = {
+  consumer_key: 'WL3koNWDA8BK5EnxJIQksxIoW',
+  consumer_secret: 'GUWKfQdXUSv22Niu99a4bBhLYC2uXjQoyYAPuz0uqQCmabAU4V',
+  access_token_key: '849071503144562692-i660Ox85clR4GjdQgNPV8ZztK6L8ZTf',
+  access_token_secret: 'exDulX4OX37uZVYUXR9hJMOz003O2UDc3SEfrnB5jB4iB',
+}
